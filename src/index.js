ee200e11bcb88823c16e9b3c6c9894d43cc7b4a7
Joose Helle <joose.helle@helsinki.fi>
2018-10-13 10:41:01 +0300

    Use styled-components for injecting global styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import './index.css'
 import App from './App'
 import store from './store'
+import { injectGlobal } from 'styled-components'
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+`
 
 const render = () => {
   ReactDOM.render(
