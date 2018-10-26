af576ce2a694e58a547c4e892fa99226ea86e81a
Joose Helle <joose.helle@helsinki.fi>
2018-10-26 09:26:10 +0300

    Refactor listening for draw events

--- src/components/map/Draw.js
+++ src/components/map/Draw.js
@@ -19,17 +19,22 @@ class Draw extends React.Component {
     map.on('load', () => map.addControl(this.draw))
     initializeDraw(this.draw)
 
-    map.on('draw.modechange', (e) => console.log('draw.modechange:', e.mode))
-    map.on('draw.selectionchange', () => drawSelectionChanged())
-    map.on('draw.delete', (e) => removeAOIs(e.features))
-    map.on('draw.update', (e) => updateDrawAreas(e))
-    map.on('draw.create', (e) => createDrawAreas(e))
+    map
+      .on('draw.modechange', (e) => console.log('draw.modechange:', e.mode))
+      .on('draw.selectionchange', () => drawSelectionChanged())
+      .on('draw.delete', (e) => removeAOIs(e.features))
+      .on('draw.update', (e) => updateDrawAreas(e))
+      .on('draw.create', (e) => createDrawAreas(e))
 
-    map.on('mouseenter', 'gl-draw-polygon-fill-inactive.cold', (e) => setMapHoveredAOI(e.features[0].properties.id))
-    map.on('mouseleave', 'gl-draw-polygon-fill-inactive.cold', () => unsetMapHoveredAOI())
-    map.on('mouseenter', 'gl-draw-polygon-fill-active.cold', (e) => setMapHoveredAOI(e.features[0].properties.id))
-    map.on('mouseleave', 'gl-draw-polygon-fill-active.cold', () => unsetMapHoveredAOI())
-    // map.on('mouseenter', 'gl-draw-polygon-fill-static.cold', (e) => { console.log('static.cold', e) })
+    map
+      .on('mouseenter', 'gl-draw-polygon-fill-inactive.cold', (e) => setMapHoveredAOI(e.features[0].properties.id))
+      .on('mouseenter', 'gl-draw-polygon-fill-active.cold', (e) => setMapHoveredAOI(e.features[0].properties.id))
+      .on('mouseenter', 'gl-draw-polygon-fill-static.cold', (e) => setMapHoveredAOI(e.features[0].properties.id))
+      .on('mouseenter', 'gl-draw-polygon-fill-inactive.hot', (e) => setMapHoveredAOI(e.features[0].properties.id))
+      .on('mouseenter', 'gl-draw-polygon-fill-active.hot', (e) => setMapHoveredAOI(e.features[0].properties.id))
+      .on('mouseenter', 'gl-draw-polygon-fill-static.hot', (e) => setMapHoveredAOI(e.features[0].properties.id))
+      .on('mouseleave', 'gl-draw-polygon-fill-inactive.cold', () => unsetMapHoveredAOI())
+      .on('mouseleave', 'gl-draw-polygon-fill-active.cold', () => unsetMapHoveredAOI())
   }
 
   render() {
