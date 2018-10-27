96779522acae49cad08dd2b5a29f70852b2b55c2
Joose Helle <joose.helle@helsinki.fi>
2018-10-27 11:43:12 +0300

    Fix errors on switching layers

--- src/components/map/AOI.js
+++ src/components/map/AOI.js
@@ -4,32 +4,47 @@ import { connect } from 'react-redux'
 class AOI extends React.Component {
 
   componentDidMount() {
-    const { layerId, FC } = this.props.aoi
-
-    this.props.map.addSource(layerId, { type: 'geojson', data: FC })
-    this.props.map.addLayer({
-      id: layerId, source: layerId, type: 'line',
-      paint: {
-        'line-width': 8,
-        'line-color': '#99f9ff',
-        'line-opacity': 0.8,
-      },
-      layout: {
-        'line-join': 'round',
-      }
-    })
+    this.addLayer()
   }
 
   componentDidUpdate = async (prevProps) => {
     const { layerId, FC, listHoveredId } = this.props.aoi
+    const { basemap } = this.props.mapState
+
+    if (prevProps.mapState.basemap !== basemap) {
+      this.props.map.on('style.load', () => {
+        this.addLayer()
+      })
+    }
 
     const layer = this.props.map.getSource(layerId)
     if (layer) layer.setData(FC)
 
-    if (listHoveredId !== '') {
+    if (!this.props.map.isStyleLoaded()) return
+
+    if (prevProps.aoi.listHoveredId !== listHoveredId) {
       this.props.map.setFilter(layerId, ['match', ['get', 'name'], listHoveredId, true, false])
     } else { this.props.map.setFilter(layerId, ['==', '-', '']) }
 
+  }
+
+  addLayer = () => {
+    const { layerId, FC } = this.props.aoi
+    if (!this.props.map.getSource(layerId)) this.props.map.addSource(layerId, { type: 'geojson', data: FC })
+    if (!this.props.map.getLayer(layerId)) {
+      this.props.map.addLayer({
+        id: layerId, source: layerId, type: 'line',
+        paint: {
+          'line-width': 8,
+          'line-color': '#99f9ff',
+          'line-opacity': 0.8,
+        },
+        layout: {
+          'line-join': 'round',
+        }
+      })
+    }
+    this.props.map.setFilter(layerId, ['==', '-', ''])
   }
 
   render() {
@@ -39,6 +54,7 @@ class AOI extends React.Component {
 
 const mapStateToProps = (state) => ({
   aoi: state.aoi,
+  mapState: state.map,
 })
 
 const ConnectedAOI = connect(mapStateToProps)(AOI)
--- src/components/map/Demo2D.js
+++ src/components/map/Demo2D.js
@@ -13,28 +13,30 @@ class Demo2D extends React.Component {
     const { layerId, visible, mbPaintStyle } = this.props.demo2d
     const { basemap } = this.props.mapState
 
-    if (visible && !prevProps.demo2d.visible) {
-      this.addGridLayer(this.props.map, layerId, censusFC, mbPaintStyle, 0)
+    if (visible && !prevProps.demo2d.visible) this.addLayer(this.props.map, layerId, censusFC, mbPaintStyle)
+
+    if (prevProps.mapState.basemap !== basemap) {
+      this.props.map.on('style.load', () => {
+        this.addLayer(this.props.map, layerId, censusFC, mbPaintStyle)
+      })
     }
-    if (visible && prevProps.mapState.basemap !== basemap) {
-      this.addGridLayer(this.props.map, layerId, censusFC, mbPaintStyle, 1)
-    }
+
     if (!visible && prevProps.demo2d.visible) {
       this.props.map.removeLayer(layerId)
-      this.props.map.removeSource(layerId)
     }
   }
 
-  addGridLayer = async (map, layerId, data, mbPaintStyle, seconds) => {
-    if (!map.isStyleLoaded()) { await new Promise(resolve => setTimeout(resolve, seconds * 1000)) }
-
-    map.addSource(layerId, { type: 'geojson', data })
-    map.addLayer({
-      id: layerId,
-      source: layerId,
-      type: 'fill',
-      paint: mbPaintStyle
-    })
+  addLayer = async (map, layerId, data, mbPaintStyle) => {
+    if (!this.props.demo2d.visible) return
+    if (!map.getSource(layerId)) map.addSource(layerId, { type: 'geojson', data })
+    if (!map.getLayer(layerId)) {
+      map.addLayer({
+        id: layerId,
+        source: layerId,
+        type: 'fill',
+        paint: mbPaintStyle
+      })
+    }
   }
 
   render() {
