cc25fefd67a97d113a95713750f30db940afc602
Joose Helle <joose.helle@helsinki.fi>
2018-10-28 21:25:51 +0200

    adjust pitch (3D)

--- src/components/map/MapControl.js
+++ src/components/map/MapControl.js
@@ -8,7 +8,7 @@ class MapControl extends React.Component {
     const { map } = this.props
     const { basemap, camera3d, zoomToBbox } = this.props.mapState
 
-    if (camera3d && !prevProps.mapState.camera3d) map.easeTo({ pitch: 35 })
+    if (camera3d && !prevProps.mapState.camera3d) map.easeTo({ pitch: 30 })
     if (!camera3d && prevProps.mapState.camera3d) map.easeTo({ pitch: 0, bearing: 0 })
     if (zoomToBbox !== prevProps.mapState.zoomToBbox) map.fitBounds(zoomToBbox)
     if (zoomToBbox !== prevProps.mapState.zoomToBbox) map.fitBounds(zoomToBbox)
